8fb79ccbc03fd70ea008a2ac553d8e4e0cbd718b
FutRR <futterermaxime@gmail.com>
2025-10-08 15:33:01 +0200

    Update AppFixtures to create unique events with varied images and enhanced descriptions

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,14 +15,18 @@ class AppFixtures extends Fixture
         $category->setName('Music');
         $manager->persist($category);
 
-        // Create 10 events and associate them with the category
-        for ($i = 1; $i <= 10; $i++) {
+        $imageDir = 'public/img/placeholder/';
+        $images = glob($imageDir . '*.webp');
+
+        // Create 5 events with different images
+        foreach ($images as $imagePath) {
             $event = new \App\Entity\Event();
-            $event->setTitle('Event ' . $i);
-            $event->setDescription('Description for event ' . $i);
-            $event->setImage('image' . $i . '.jpg');
-            $event->setDatetimeStart(new \DateTime('2024-07-' . str_pad($i, 2, '0', STR_PAD_LEFT) . ' 18:00:00'));
-            $event->setDatetimeEnd(new \DateTime('2024-07-' . str_pad($i, 2, '0', STR_PAD_LEFT) . ' 21:00:00'));
+            $event->setTitle('Event' . uniqid());
+            $event->setDescription("This event is a unique opportunity to discover new musical talents and enjoy a festive atmosphere. Come and experience a variety of performances and meet other music enthusiasts.
+            The evening will take place in a friendly setting, with entertainment and surprises throughout the event. Don't forget to invite your friends to share this exceptional moment!");
+            $event->setImage('/img/placeholder/' . basename($imagePath)); // URL path
+            $event->setDatetimeStart(new \DateTime('2024-07-01 18:00:00'));
+            $event->setDatetimeEnd(new \DateTime('2024-07-01 21:00:00'));
             $event->setCategory($category);
             $manager->persist($event);
         }
