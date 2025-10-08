6696ce338c315b408e2c895b759a0697dc2c56da
FutRR <futterermaxime@gmail.com>
2025-10-08 10:39:37 +0200

    Add Category and Event entities with repositories for event management

---
+++ src/Entity/Event.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column]
+    private ?\DateTime $datetime_start = null;
+
+    #[ORM\Column]
+    private ?\DateTime $datetime_end = null;
+
+    #[ORM\ManyToOne(inversedBy: 'events')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDatetimeStart(): ?\DateTime
+    {
+        return $this->datetime_start;
+    }
+
+    public function setDatetimeStart(\DateTime $datetime_start): static
+    {
+        $this->datetime_start = $datetime_start;
+
+        return $this;
+    }
+
+    public function getDatetimeEnd(): ?\DateTime
+    {
+        return $this->datetime_end;
+    }
+
+    public function setDatetimeEnd(\DateTime $datetime_end): static
+    {
+        $this->datetime_end = $datetime_end;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
