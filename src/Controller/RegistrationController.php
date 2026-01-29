bbd4732157a25b3f93e48e385b3521d33dc7e119
FutRR <futterermaxime@gmail.com>
2026-01-29 11:37:53 +0100

    email verifier issue v2

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -41,13 +41,13 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('mailer@futevent.com', 'FutEvent'))
-                    ->to((string) $user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+//            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+//                (new TemplatedEmail())
+//                    ->from(new Address('mailer@futevent.com', 'FutEvent'))
+//                    ->to((string) $user->getEmail())
+//                    ->subject('Please Confirm your Email')
+//                    ->htmlTemplate('registration/confirmation_email.html.twig')
+//            );
 
             // do anything else you need here, like send an email
 
